53f32087ec40cd8037f263e29e652c6a1b34649d
Ryan Buchmayer <buchmr@uw.edu>
2018-06-24 19:32:14 -0700

    v1.0

--- src/datastructures/worklists/ArrayStack.java
+++ src/datastructures/worklists/ArrayStack.java
@@ -1,40 +1,77 @@
 package datastructures.worklists;
 
+import java.util.NoSuchElementException;
+
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.worklists.LIFOWorkList;
 
 /**
- * See cse332/interfaces/worklists/LIFOWorkList.java
- * for method specifications.
+ * See cse332/interfaces/worklists/LIFOWorkList.java for method specifications.
  */
 public class ArrayStack<E> extends LIFOWorkList<E> {
 
-    public ArrayStack() {
-        throw new NotYetImplementedException();
-    }
+	private E[] arr;
+	private int back;
+	private int size;
 
-    @Override
-    public void add(E work) {
-        throw new NotYetImplementedException();
-    }
+	@SuppressWarnings("unchecked")
+	public ArrayStack() {
+		arr = (E[]) new Object[10];
+		back = -1;
+		size = 0;
+	}
 
-    @Override
-    public E peek() {
-        throw new NotYetImplementedException();
-    }
+	@Override
+	public void add(E work) {
+		try {
+			back++;
+			arr[back] = work;
+			size++;
+		} catch (IndexOutOfBoundsException resize) {
+			
+			@SuppressWarnings("unchecked")
+			E[] resized = (E[]) new Object[arr.length * 2]; 
+			for(int i = 0; i < arr.length; i++) {
+				resized[i] = arr[i];
+			}
+			arr = resized;
+			back++;
+			size++;
+			arr[back] = work;
+		}
 
-    @Override
-    public E next() {
-        throw new NotYetImplementedException();
-    }
+	}
 
-    @Override
-    public int size() {
-        throw new NotYetImplementedException();
-    }
+	@Override
+	public E peek() {
+		if(size <= 0) {
+			throw new NoSuchElementException();
+		}
+		E data = arr[back];
+		return data;
+	}
 
-    @Override
-    public void clear() {
-        throw new NotYetImplementedException();
-    }
+	@Override
+	public E next() {
+		if(size <= 0) {
+			throw new NoSuchElementException();
+		}
+		E data = arr[back];
+		back--;
+		size--;
+		return data;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void clear() {
+		arr = (E[]) new Object[10];
+		back = 0;
+		size = 0;
+	}
 }
--- src/datastructures/worklists/ListFIFOQueue.java
+++ src/datastructures/worklists/ListFIFOQueue.java
@@ -1,40 +1,78 @@
 package datastructures.worklists;
 
-import cse332.exceptions.NotYetImplementedException;
+import java.util.NoSuchElementException;
+
 import cse332.interfaces.worklists.FIFOWorkList;
 
 /**
- * See cse332/interfaces/worklists/FIFOWorkList.java
- * for method specifications.
+ * See cse332/interfaces/worklists/FIFOWorkList.java for method specifications.
  */
 public class ListFIFOQueue<E> extends FIFOWorkList<E> {
-    
-    public ListFIFOQueue() {
-        throw new NotYetImplementedException();
-    }
 
-    @Override
-    public void add(E work) {
-        throw new NotYetImplementedException();
-    }
+	@SuppressWarnings("hiding")
+	private class Node<E> {
+		private E data;
+		private Node<E> next;
+	}
 
-    @Override
-    public E peek() {
-        throw new NotYetImplementedException();
-    }
+	private Node<E> front;
+	private Node<E> back;
 
-    @Override
-    public E next() {
-        throw new NotYetImplementedException();
-    }
+	public ListFIFOQueue() {
+		front = null;
+		back = front;
+	}
 
-    @Override
-    public int size() {
-        throw new NotYetImplementedException();
-    }
+	@Override
+	public void add(E work) {
+		Node<E> n = new Node<E>();
+		n.data = work;
+		n.next = null;
+		if (front == null) {
+			front = n;
+			back = n;
+		} else {
+			back.next = n;
+			back = n;
+		}
+	}
 
-    @Override
-    public void clear() {
-        throw new NotYetImplementedException();
-    }
+	@Override
+	public E peek() {
+		if(front == null) {
+			throw new NoSuchElementException();
+		}
+		E data = front.data;
+		return data;
+	}
+
+	@Override
+	public E next() {
+		if(front == null) {
+			throw new NoSuchElementException();
+		}
+		E next = front.data;
+		front = front.next;
+		if(front == null) {
+			back = null;
+		}
+		return next;
+	}
+
+	@Override
+	public int size() {
+		Node<E> current = front;
+		int size = 0;
+		while (current != null) {
+			current = current.next;
+			size++;
+		}
+		return size;
+	}
+
+	@Override
+	public void clear() {
+		front = null;
+		back = front;
+	}
 }
